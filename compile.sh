9aeffff5013a6b92a964872d7dbb99d2d82f887e
JeremiasEzeq <jeremiasezequiel.alvarenga@rai.usc.es>
2026-04-23 02:28:58 +0200

    Avanza scripts de generación de FID
    Los scripts aún no están correctamente adaptados al procesamiento de binarios directo

--- compile.sh
+++ compile.sh
@@ -1,2 +1,14 @@
-gcc openssl_test.c libcrypto.a -o openssl_test -fvisibility=hidden
-strip --strip-all openssl_test
+wget https://github.com/openssl/openssl/releases/download/openssl-4.0.0/openssl-4.0.0.tar.gz
+tar -xf openssl-4.0.0.tar.gz
+cd openssl-4.0.0
+./Configure -static
+make
+cd ..
+cp openssl-4.0.0/apps/openssl .
+
+./01-unpack-debs.sh debs openssl
+
+# Generación de valor dummie para iterar sobre el único ejecutable
+echo "test/test/unknown/x86-64" >> libs/all_libs
+
+./02-generate-fidb.sh ~/ghidra_home
